f870eba6086e8d3baba90e9ecb43f77e368a9b35
Xiaoxiang.Wang <wangxiaoxiang@youku.com>
2016-11-15 16:30:47 +0800

    pod support

---
+++ releases/iOS/LFModelKit.framework/Headers/LFModelKit.h
@@ -0,0 +1,22 @@
+//
+//  LFModelKit.h
+//  LFModelKit
+//
+//  Created by 汪潇翔 on 27/10/2016.
+//  Copyright © 2016 汪潇翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LFModelKit.
+FOUNDATION_EXPORT double LFModelKitVersionNumber;
+
+//! Project version string for LFModelKit.
+FOUNDATION_EXPORT const unsigned char LFModelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LFModelKit/PublicHeader.h>
+
+ #import <LFModelKit/LFClassInfo.h>
+ #import <LFModelKit/LFNSObject+Model.h>
+
+
